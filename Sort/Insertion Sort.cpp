ae86dc8f0080bd0c34806421fc26d2217aae45b1
ChaCha3088 <cha3088@gmail.com>
2023-04-24 17:27:36 +0900

    Insertion Sort

---
+++ Sort/Insertion Sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#define fastIo cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+
+using namespace std;
+
+int arr[5];
+int iter = 5;
+int keyValue;
+
+int main() {
+    fastIo;
+
+    //입력
+    FILE *pFile = freopen("sortInput.txt", "r", stdin);
+
+    for (int i = 0; i < iter; i++) {
+        cin >> arr[i];
+    }
+
+    //삽입 정렬
+    //카드를 정렬하는 방법과 유사
+    //시간복잡도
+    //최악 O(n^2)
+    //평균 O(n^2)
+    //최상 O(n)
+    //공간복잡도 O(1)
+    for (int i = 1; i < iter; ++i) {
+        keyValue = arr[i];
+
+        for (int j = i - 1; j >= 0 & arr[j] > keyValue; --j) {
+            arr[j + 1] = arr[j];
+            arr[j] = keyValue;
+        }
+
+        //출력
+        for (int k = 0; k < iter; k++) {
+            cout << arr[k];
+            if (k != iter - 1) {
+                cout << " ";
+            } else if (k == iter - 1) {
+                cout << "\n";
+            }
+        }
+    }
+}
